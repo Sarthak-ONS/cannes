46a4bdbdb2299b25dd10560eb73ab0042425a7b8
Sarthak Agarwak <agarwalsarthak456@gmail.com>
2022-08-16 21:22:39 +0530

    Added New arrivals

--- lib/Widgets/new_arrival_widget.dart
+++ lib/Widgets/new_arrival_widget.dart
@@ -21,7 +21,7 @@ class _NewArrivalState extends State<NewArrival> {
   Widget build(BuildContext context) {
     return Container(
       height: 300,
-      color: Colors.red,
+      color: Colors.white,
       child: Column(
         children: [
           SizedBox(
@@ -32,14 +32,57 @@ class _NewArrivalState extends State<NewArrival> {
               itemBuilder: (BuildContext context, int index) {
                 return Padding(
                   padding: const EdgeInsets.all(8.0),
-                  child: Chip(
-                    label: Text(newArrivalChips[index]),
+                  child: CustomRawChip(
+                    newArrivalChips: newArrivalChips,
+                    index: index,
                   ),
                 );
               },
             ),
           )
         ],
+      ),
+    );
+  }
+}
+
+class CustomRawChip extends StatefulWidget {
+  const CustomRawChip({
+    Key? key,
+    this.index,
+    required this.newArrivalChips,
+  }) : super(key: key);
+
+  final List newArrivalChips;
+  final int? index;
+
+  @override
+  State<CustomRawChip> createState() => _CustomRawChipState();
+}
+
+class _CustomRawChipState extends State<CustomRawChip> {
+  bool isChippedTapped = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return RawChip(
+      side: !isChippedTapped
+          ? const BorderSide(color: Colors.black, width: 0.5)
+          : null,
+      onPressed: () {
+        setState(() {
+          isChippedTapped = !isChippedTapped;
+        });
+      },
+      elevation: 1,
+      backgroundColor: isChippedTapped ? Colors.red : Colors.white,
+      label: Text(
+        widget.newArrivalChips[widget.index!],
+        style: TextStyle(
+          fontSize: 12,
+          fontWeight: FontWeight.w300,
+          color: isChippedTapped ? Colors.white : Colors.black,
+        ),
       ),
     );
   }
